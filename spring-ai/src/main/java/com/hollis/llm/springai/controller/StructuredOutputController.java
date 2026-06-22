ce4d6af07db7efe361d73d61c7720c78f629e0ca
啷个哩个啷 <>
2026-06-22 16:06:26 +0800

    feat:补充结构化输出

--- spring-ai/src/main/java/com/hollis/llm/springai/controller/StructuredOutputController.java
+++ spring-ai/src/main/java/com/hollis/llm/springai/controller/StructuredOutputController.java
@@ -7,13 +7,17 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -50,6 +54,35 @@ public class StructuredOutputController implements InitializingBean {
                 .call().entity(Book.class);
         System.out.println(book.toString());
         return book.name() + " 、 " + book.author() + " 、 " + book.desc() + " 、 " + book.price() + " 、 " + book.publisher();
+    }
+
+
+    @RequestMapping("/convertList")
+    public String convertList() {
+        List<String> book = chatClient.prompt("请给我推荐几本心理学有关的书")
+                .call().entity(new ListOutputConverter());
+        System.out.println(book.toString());
+        return "success";
+    }
+
+    @RequestMapping("/convertList2")
+    public String convertList2() {
+        List<Book> book = chatClient.prompt("请给我推荐几本心理学有关的书")
+                .call().entity(new ParameterizedTypeReference<>() {
+                });
+
+        System.out.println(book);
+        return book.toString();
+    }
+
+
+    @RequestMapping("/z")
+    public String convertMap() {
+        Map<String, Object> book = chatClient.prompt("请给我推荐几本心理学有关的书，书的内容包括书名、作者、价格、上市时间等信息，以书名作为key，书的信息作为value")
+                .call().entity(new MapOutputConverter());
+
+        System.out.println(book);
+        return book.toString();
     }
 
 
